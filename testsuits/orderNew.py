5aebe5701c1605b27b855b21b7e9fa00b78be347
kk <kayiyo@live.cn>
2017-10-13 14:43:58 +0800

    update read order info

--- testsuits/orderNew.py
+++ testsuits/orderNew.py
@@ -6,13 +6,14 @@ __author__ = 'kayiyo'
 import ConfigParser
 import os.path
 import time
+import random
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 from framework.logger import Logger
 
-mylogger = Logger(logger="bossOrderLog").getlog()
+mylogger = Logger(logger="orderNewLog").getlog()
 config = ConfigParser.ConfigParser()
 # file_path = os.path.dirname(os.getcwd()) + '/config/config.ini'
 file_path = os.path.dirname(os.path.abspath('.')) + '/config/configuration.ini'
@@ -83,10 +84,12 @@ def neworder():
     time.sleep(2)
 
     # PO号
+    global orderpono
+    orderpono = "PO" + ordertime1 + "ddgl"
     inputbox = driver.find_element_by_xpath(
         ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[2]/div[2]/span[2]/div/div/div[1]/input")
     inputbox.clear()
-    inputbox.send_keys("PO" + ordertime1 + "ddgl")
+    inputbox.send_keys(orderpono)
 
     # 订单签约日期
     inputbox = driver.find_element_by_xpath(
@@ -139,10 +142,11 @@ def neworder():
     # inputbox.send_keys("xujie@group.com")
 
     # 输入订单金额
+    orderaccount = random.randint(1, 10) * 1000000
     inputbox = driver.find_element_by_xpath(
         ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[9]/div[1]/span[2]/div/div/div[1]/input")
     inputbox.clear()
-    inputbox.send_keys("3000000")
+    inputbox.send_keys(orderaccount)
 
     # 选择货币
     select = driver.find_element_by_xpath(
@@ -157,7 +161,7 @@ def neworder():
     select = driver.find_element_by_xpath(
         ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[10]/div[1]/span[2]/div/div/div/div[1]/input")
     select.click()
-    for iii in range(1, 8):
+    for iii in range(1, random.randint(2, 10)):
         select.send_keys(Keys.UP)
     select.send_keys(Keys.ENTER)
     time.sleep(0.5)
@@ -166,7 +170,7 @@ def neworder():
     select = driver.find_element_by_xpath(
         ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[10]/div[2]/span[2]/div/div/div/div[1]/input")
     select.click()
-    for iii in range(1, 7):
+    for iii in range(1, random.randint(2, 10)):
         select.send_keys(Keys.UP)
     select.send_keys(Keys.ENTER)
     time.sleep(0.5)
@@ -175,7 +179,7 @@ def neworder():
     select = driver.find_element_by_xpath(
         ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[11]/div[1]/span[2]/div/div/div[1]/div[1]/input")
     select.click()
-    for iii in range(1, 12):
+    for iii in range(1, random.randint(2, 20)):
         select.send_keys(Keys.UP)
     select.send_keys(Keys.ENTER)
     time.sleep(0.5)
@@ -184,7 +188,7 @@ def neworder():
     select = driver.find_element_by_xpath(
         ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[11]/div[2]/span[2]/div/div/div[1]/div[1]/input")
     select.click()
-    for iii in range(1, 3):
+    for iii in range(1, random.randint(2, 20)):
         select.send_keys(Keys.UP)
     select.send_keys(Keys.ENTER)
     time.sleep(0.5)
@@ -199,16 +203,79 @@ def neworder():
     driver.find_element_by_xpath(".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/div[1]/button").click()
     time.sleep(2)
 
-    # 读取平台订单号
-    ordercode = driver.find_element_by_xpath(
-        ".//*[@id='app']/div[2]/div[2]/div/div[1]/div[3]/table/tbody/tr[1]/td[1]/div").text
-    mylogger.info(u"平台订单号：" + ordercode)
+    # # 读取平台订单号
+    # ordercode = driver.find_element_by_xpath(
+    #     ".//*[@id='app']/div[2]/div[2]/div/div[1]/div[3]/table/tbody/tr[1]/td[1]/div").text
+    # mylogger.info(u"平台订单号：" + ordercode)
+
+def orderinfo():
+
+        # 重置搜索条件
+        driver.find_element_by_xpath(".//*[@id='app']/div[2]/div[2]/div/form/div/div[8]/div/div/div/button[2]").click()
+        # 输入平台订单号
+        driver.find_element_by_xpath(
+            ".//*[@id='app']/div[2]/div[2]/div/form/div/div[4]/div/div/div/div/input").send_keys(
+            orderpono)
+        # 点击搜索按钮
+        driver.find_element_by_xpath(".//*[@id='app']/div[2]/div[2]/div/form/div/div[8]/div/div/div/button[1]").click()
+
+        try:
+            driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[1]/div[4]/div[2]/table/tbody/tr/td[9]/div/button[1]")
+        except Exception as e:
+            # mylogger.info(e)
+            mylogger.info(u"PO：" + orderpono + u"不存在！")
+        else:
+            # 查看该订单
+            driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[1]/div[4]/div[2]/table/tbody/tr/td[9]/div/button[1]").click()
+
+            # 读取PO号
+            readpo = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[2]/div[2]/span[2]/input").text
+
+            # 读取平台订单号
+            readorderno = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[2]/div[1]/span[2]").text
+
+            # 读取客户编号
+            readcusno = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[4]/div[1]/span[2]").text
+
+            # 读取市场经办人
+            readmarket = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[4]/div[2]/span[2]").text
+
+            # 读取订单金额
+            readorderaccount = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[9]/div[1]/span[2]").text
+
+            # 读取贸易条款
+            readclause = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[10]/div[1]/span[2]").text
+
+            # 读取运输方式
+            readway = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[10]/div[2]/span[2]").text
+
+            # 读取起运地
+            readstart = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[11]/div[1]/span[2]").text
+
+            # 读取目的地
+            readend = driver.find_element_by_xpath(
+                ".//*[@id='app']/div[2]/div[2]/div/div[2]/div[1]/form/div[11]/div[2]/span[2]").text
+
+            readall = u"|PO." + readpo + u"|客户编号:" + readcusno + u"|经办人:" + readmarket + u"|订单金额:" + readorderaccount + u"|clause:" + readclause + u"|起运方式:" + readway + u"[" + readstart + u"-->" + readend + u"]"
+
+            mylogger.info(u"平台订单号：" + readorderno)
+            mylogger.info(readall)
 
 
 testTime = int(config.get("bossOrder", "testtime"))
 url = config.get("bossOrder", "url")
 mylogger.info("The test server url is: %s" % url)
-mylogger.info("TestTask : bossOrder")
+mylogger.info("TestTask : orderNew")
 
 testTimes = testTime + 1
 mylogger.info("Test Time is : %s" % testTime)
@@ -234,6 +301,9 @@ for iii in range(1, testTimes):
 
     # 新建订单
     neworder();
+
+    # 读取订单
+    orderinfo();
 
     # 退出
     logout();
